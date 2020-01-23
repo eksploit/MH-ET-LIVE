ce0909a95ce3a7665f594072ef18ec43874f47f5
eksploit <60088291+eksploit@users.noreply.github.com>
2020-01-23 16:33:42 +0300

    Update EPD.h

--- Inc/EPD.h
+++ Inc/EPD.h
@@ -1,7 +1,7 @@
 /*
  * EPD.h
  *
- *  Created on: 21 ÿíâ. 2020 ã.
+ *  Created on: 21 янв. 2020 г.
  *      Author: User
  */
 
@@ -13,10 +13,10 @@
 
 #define RESET_TIME	200
 
-#define EPD_RST_UP		LL_GPIO_ResetOutputPin(RST_GPIO_Port, RST_Pin)
+#define EPD_RST_UP	LL_GPIO_ResetOutputPin(RST_GPIO_Port, RST_Pin)
 #define EPD_RST_DOWN	LL_GPIO_SetOutputPin(RST_GPIO_Port, RST_Pin)
 
-#define EPD_DC_UP 		LL_GPIO_SetOutputPin(DC_GPIO_Port, DC_Pin)
+#define EPD_DC_UP 	LL_GPIO_SetOutputPin(DC_GPIO_Port, DC_Pin)
 #define EPD_DC_DOWN 	LL_GPIO_ResetOutputPin(DC_GPIO_Port, DC_Pin)
 
 #endif /* EPD_H_ */
@@ -37,9 +37,9 @@ void EPD_SendCommand(uint8_t command);
 void EPD_SendData(uint8_t command);
 void EPD_Paint_Screen(color background);
 void EPD_Buff_Screen(uint8_t * buff, uint16_t len, color background);
-//ñîçäàíèå ìàññèâà ñèìâîëîâ str
+//создание массива символов str
 void EPD_string_to_ACSII(char *str, uint8_t *buf);
-//âûâîäèì íà ýêðàí ñèìâîëû
+//выводим на экран символы
 void EPD_string_paint(char *str);
 void Busy();
 
