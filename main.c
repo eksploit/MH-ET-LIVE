f985053e832e84102b43db380b12037cae8ef615
eksploit <60088291+eksploit@users.noreply.github.com>
2020-01-23 16:15:33 +0300

    Add files via upload (#1)
    * Add files via upload
    
    * Create Inc
    
    * Delete Inc

--- main.c
+++ main.c
@@ -11,10 +11,12 @@
 /* USER CODE END Header */
 
 /* Includes ------------------------------------------------------------------*/
+#include <stdlib.h>
+#include <time.h>
 #include "main.h"
 #include "spi.h"
 #include "gpio.h"
-
+#include "EPD.h"
 /* Private includes ----------------------------------------------------------*/
 /* USER CODE BEGIN Includes */
 
@@ -27,9 +29,7 @@
 
 /* Private define ------------------------------------------------------------*/
 /* USER CODE BEGIN PD */
-#define RESET_TIME	200
-#define WHITE	0xFF
-#define BLACK	0x00
+
 /* USER CODE END PD */
 
 /* Private macro -------------------------------------------------------------*/
@@ -84,147 +84,16 @@ int main(void)
   MX_GPIO_Init();
   MX_SPI1_Init();
   /* USER CODE BEGIN 2 */
-  uint8_t data;
   //----------------RESET
-  	LL_mDelay(RESET_TIME);
-  	LL_GPIO_ResetOutputPin(RST_GPIO_Port, RST_Pin);
-  	LL_mDelay(RESET_TIME);
-  	LL_GPIO_SetOutputPin(RST_GPIO_Port, RST_Pin);
-  	LL_mDelay(RESET_TIME);
-  	//-------------- Driver Output control
-  	LL_GPIO_ResetOutputPin(DC_GPIO_Port, DC_Pin);
-  	data = 0x01;
-  	HAL_SPI_Transmit(&hspi1, &data, 1, 1);
-		//display height
-		LL_GPIO_SetOutputPin(DC_GPIO_Port, DC_Pin);
-		data = 0xc7;
-		HAL_SPI_Transmit(&hspi1, &data, 1, 1);
-		data = 0x00;
-		HAL_SPI_Transmit(&hspi1, &data, 1, 1);
-		//GD  SM  TB
-		HAL_SPI_Transmit(&hspi1, &data, 1, 1);
-  	//------------------
-  	//--------------Gate Driving voltage Control
-  	LL_GPIO_ResetOutputPin(DC_GPIO_Port, DC_Pin);
-  	data = 0x03;
-  	HAL_SPI_Transmit(&hspi1, &data, 1, 1);
+  EPD_Reset();
+  EPD_Init();
 
-		LL_GPIO_SetOutputPin(DC_GPIO_Port, DC_Pin);
-		data = 0xEA;
-		HAL_SPI_Transmit(&hspi1, &data, 1, 1);
-		LL_GPIO_ResetOutputPin(DC_GPIO_Port, DC_Pin);
-  	//--------------
-  	//--------------Source Driving	voltage Control
-	data = 0x04;
-	HAL_SPI_Transmit(&hspi1, &data, 1, 1);
+  EPD_Paint_Screen(black);
+  EPD_Paint_Screen(white);
+  //EPD_Buff_Screen((uint8_t*)&gImage_b1, sizeof(gImage_b1), black);
+  EPD_string_paint("F.E.A.R.F.E.A.R.F.E.A.R.F.E.A.R.F.E.A.R.F.E.A.R.F.E.A.R.F.E.A.R.F.E.A.R.F.E.A.R.   1234567890   …÷” ≈Õ√ÿŸ«’⁄‘€¬®¿œ–ŒÀƒ∆›ﬂ◊—Ã»“‹¡ﬁ QWERTYUIOPASDFGHJKLZXCVBNM ÈˆÛÍÂÌ„¯˘Áı˙Ù˚‚‡ÔÓÎ‰Ê˝ˇ˜ÒÏËÚ¸·˛ qwertyuiopasdfghjklzxcvbnm");
 
-		LL_GPIO_SetOutputPin(DC_GPIO_Port, DC_Pin);
-		data = 0x0A;
-		HAL_SPI_Transmit(&hspi1, &data, 1, 1);
-		LL_GPIO_ResetOutputPin(DC_GPIO_Port, DC_Pin);
-	//--------------
-	//--------------Set dummy line period
-	data = 0x3A;
-	HAL_SPI_Transmit(&hspi1, &data, 1, 1);
-		LL_GPIO_SetOutputPin(DC_GPIO_Port, DC_Pin);
-		data = 0x02;
-		HAL_SPI_Transmit(&hspi1, &data, 1, 1);
-		LL_GPIO_ResetOutputPin(DC_GPIO_Port, DC_Pin);
-	//--------------
-	//--------------Set Gate line width
-	data = 0x3B;
-	HAL_SPI_Transmit(&hspi1, &data, 1, 1);
-		LL_GPIO_SetOutputPin(DC_GPIO_Port, DC_Pin);
-		data = 0x08;
-		HAL_SPI_Transmit(&hspi1, &data, 1, 1);
-		LL_GPIO_ResetOutputPin(DC_GPIO_Port, DC_Pin);
-	//--------------
-	//--------------Border Waveform Control
-	data = 0x3C;
-	HAL_SPI_Transmit(&hspi1, &data, 1, 1);
-		LL_GPIO_SetOutputPin(DC_GPIO_Port, DC_Pin);
-		data = 0x71;
-		HAL_SPI_Transmit(&hspi1, &data, 1, 1);
-		LL_GPIO_ResetOutputPin(DC_GPIO_Port, DC_Pin);
-	//--------------
-	//--------------Data Entry mode setting
-	data = 0x11;
-	HAL_SPI_Transmit(&hspi1, &data, 1, 1);
-		LL_GPIO_SetOutputPin(DC_GPIO_Port, DC_Pin);
-		data = 0x03;
-		HAL_SPI_Transmit(&hspi1, &data, 1, 1);
-		LL_GPIO_ResetOutputPin(DC_GPIO_Port, DC_Pin);
-	//--------------
-	//--------------Set RAM X - address Start / End position
-	data = 0x44;
-	HAL_SPI_Transmit(&hspi1, &data, 1, 1);
-		LL_GPIO_SetOutputPin(DC_GPIO_Port, DC_Pin);
-		data = 0x00; //xDot -start
-		HAL_SPI_Transmit(&hspi1, &data, 1, 1);
-		data = 0x18; //xDot/8 -end
-		HAL_SPI_Transmit(&hspi1, &data, 1, 1);
-		LL_GPIO_ResetOutputPin(DC_GPIO_Port, DC_Pin);
-	//--------------Set RAM X - address Start / End position
-	data = 0x45;
-	HAL_SPI_Transmit(&hspi1, &data, 1, 1);
-		LL_GPIO_SetOutputPin(DC_GPIO_Port, DC_Pin);
-		data = 0x00; //yDot/8 start
-		HAL_SPI_Transmit(&hspi1, &data, 1, 1);
-		data = 0x00; //yDot/8 start
-		HAL_SPI_Transmit(&hspi1, &data, 1, 1);
-		data = 0xC7; //yDot%256
-		HAL_SPI_Transmit(&hspi1, &data, 1, 1);
-		data = 0x00; //yDot/256
-		HAL_SPI_Transmit(&hspi1, &data, 1, 1);
-		LL_GPIO_ResetOutputPin(DC_GPIO_Port, DC_Pin);
-	//--------------
-	//--------------Set RAM X address counter
-	data = 0x4E;
-	HAL_SPI_Transmit(&hspi1, &data, 1, 1);
-		LL_GPIO_SetOutputPin(DC_GPIO_Port, DC_Pin);
-		data = 0x00; //
-		HAL_SPI_Transmit(&hspi1, &data, 1, 1);
-		LL_GPIO_ResetOutputPin(DC_GPIO_Port, DC_Pin);
-	//--------------
-	//--------------Set RAM X address counter
-	data = 0x4F;
-	HAL_SPI_Transmit(&hspi1, &data, 1, 1);
-		LL_GPIO_SetOutputPin(DC_GPIO_Port, DC_Pin);
-		data = 0x00; //
-		HAL_SPI_Transmit(&hspi1, &data, 1, 1);
-		data = 0x00; //
-		HAL_SPI_Transmit(&hspi1, &data, 1, 1);
-		LL_GPIO_ResetOutputPin(DC_GPIO_Port, DC_Pin);
-	//--------------Write RAM
-	data = 0x24;
-	HAL_SPI_Transmit(&hspi1, &data, 1, 1);
-	LL_GPIO_SetOutputPin(DC_GPIO_Port, DC_Pin);
-	data = BLACK;
-	int i;
-	for(i = 0; i < 2500; i++)
-	{
-		HAL_SPI_Transmit(&hspi1, &data, 1, 1);
-
-	}
-	data = WHITE;
-	for(i = 0; i < 2500; i++)
-	{
-		HAL_SPI_Transmit(&hspi1, &data, 1, 1);
-	}
-	LL_GPIO_ResetOutputPin(DC_GPIO_Port, DC_Pin);
-	//--------------
-	//--------------Display Update  Control 2
-	data = 0x22;
-	HAL_SPI_Transmit(&hspi1, &data, 1, 10);
-		LL_GPIO_SetOutputPin(DC_GPIO_Port, DC_Pin);
-		data = 0xFF; //
-		HAL_SPI_Transmit(&hspi1, &data, 1, 10);
-		LL_GPIO_ResetOutputPin(DC_GPIO_Port, DC_Pin);
-	//--------------
-	//--------------Master Activation
-	data = 0x20;
-	HAL_SPI_Transmit(&hspi1, &data, 1, 10);
-
+asm("nop");
   /* USER CODE END 2 */
 
   /* Infinite loop */
